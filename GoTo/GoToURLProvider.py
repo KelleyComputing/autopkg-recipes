8302aa1d8b877c80dc6b2cbe8124c2bf2e3570f2
Quanah Green <quanahgreen@gmail.com>
2023-04-25 14:11:35 -0700

    Use a single GoTo recipe that can be overridden for arm or x86

---
+++ GoTo/GoToURLProvider.py
@@ -0,0 +1,44 @@
+
+from autopkglib import ProcessorError, Processor
+
+__all__ = ["GoToURLProvider"]
+
+class GoToURLProvider(Processor):
+    """Provides an architecture specific download URL for the latest GoTo Version"""
+
+    input_variables = {
+        "architecture": {
+            "required": False,
+            "description": "Architecture of the R package to download. "
+            "Possible values are 'x86_64' (Intel) or 'arm64' (Apple Silicon). "
+            "Defaults to 'x86_64' (Intel).",
+        },
+    }
+    output_variables = {
+        "url": {"description": "URL to the latest R release."},
+    }
+    description = __doc__
+
+    def main(self):
+        """Main process."""
+
+        # Set Base Url
+        url_base = "https://goto-desktop.goto.com/"
+
+        # Read and validate input variables
+        arch = self.env.get("architecture", "x86_64")
+        if arch not in ("x86_64", "arm64"):
+            raise ProcessorError("Architecture must be one of: x86_64, arm64")
+
+        # Set url in environment
+        if arch == "x86_64":
+            self.env["url"] = url_base + "GoTo.dmg"
+        elif arch == "arm64":
+            self.env["url"] = url_base + "GoTo-arm64.dmg"
+
+        self.output("Found url: %s" % self.env["url"])
+
+
+if __name__ == "__main__":
+    PROCESSOR = GoToURLProvider()
+    PROCESSOR.execute_shell()
